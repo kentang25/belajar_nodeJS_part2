a74e69e50b878ed1ff776add2436574dc1511cbc
REGI RIDHO <regiyanuar123@gmail.com>
2026-04-19 01:05:28 +0700

    belajar express part2

--- belajar_express.js
+++ belajar_express.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+app.post('/', (req, res) => {
+    res.send('Hello Post');
 });
 
+app.get('/about', (req, res) => {
+    res.send('Hello About');
+});
+
+// app.all('*', (req, res) => {
+//     res.status(404).send('404 Not Found');
+// });
+
+
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
